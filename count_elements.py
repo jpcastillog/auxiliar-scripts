74cb86e40be7bfca3927a98c52b63f61ab8a5295
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2022-02-23 17:05:25 -0300

    Update count_elements.py

--- count_elements.py
+++ count_elements.py
@@ -11,9 +11,9 @@ def countInvertedLists(path, min_size):
         n_il = 0
         if size > min_size:
             n_il += 1
-            print('Inverted lists > {0} -> {1}'.format(min_size, n_il))
+            print('Inverted lists > ', min_size,  '-> ', n_il)
     f.close()
-    print('Total # inverted lists: {0}'.format(n_il))
+    print('Total # inverted lists: ', n_il)
     return n_il
 
 
