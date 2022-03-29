7ba78fd282552f0874d15beb888fc884fb1d24f2
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2022-03-28 22:51:24 -0300

    Update filter_index.py

--- filter_index.py
+++ filter_index.py
@@ -1,5 +1,7 @@
 from audioop import add
+from distutils.fancy_getopt import OptionDummy
 from importlib.resources import path
+from os import remove
 import struct
 from struct import *
 
@@ -15,7 +17,38 @@ def addUniverse(u, file):
     f.close()
     print('FIN !!!!')
 
+def removeElementsQueries(min_id, queries_file, out_file):
+    f = open(file, 'r')
+    filtered_queries = []
+    nq = 0 
+    for line in f:
+        elements = line.strip().split(' ')
+        elements = list(map(int, elements))
+        flag = True
+        for e in elements:
+            if e > min_id:
+                flag = False
+                break
+        if flag:
+            filtered_queries.append(elements)
+        nq += 1
+    f.close()
+    print(f'Total queries readed: {nq}')
+    print(f'Writing {len(filtered_queries)} filtered queries!')
+    fo = open(out_file, 'w')
+    for q in filtered_queries:
+        str_q = ' '.join(str(x) for x in q)
+        fo.write(str_q)
+    fo.close()
+
+
+
+
+
 # path = 'D:/data/Gov2Flat/gov2.unsorted'
-path = 'D:/data/ClueWeb09Flat/clueweb09.sorted'
+# path = 'D:/data/ClueWeb09Flat/clueweb09.sorted'
+in_path = 'D:/Data/Gov2Flat/1mq.txt'
+out_path = 'D:/Data/Gov2Flat/1mq_filtered.txt'
 u = 50131015
-addUniverse(u, path)
+# addUniverse(u, path)
+removeElementsQueries(57224, in_path, out_path)
